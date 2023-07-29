d5278b6e4cef7016d88c091934562aeeca52f73d
Racun77 <engelmann.maik@gmail.com>
2023-07-30 00:06:42 +0200

    Player Stats lvl-up UI fertig

--- Assets/Scripts/PlayerStatUpgradeDisplay.cs
+++ Assets/Scripts/PlayerStatUpgradeDisplay.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerStatUpgradeDisplay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public TMP_Text valueText, coastText;
+    public GameObject upgradeButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
